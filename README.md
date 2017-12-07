f13bf62199a222bf5e999b41094bfe82e0ae2ca2
MadInc63 <MadInc63@gmail.com>
2017-12-07 10:36:19 +0400

    alpha

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The script displays a list of users who post their code between 00:00 and 06:00
+
+# How to use
+
+Run the script using Python 3.5 from the command line and the script will display information using the devman.org api
+
+```Bash
+$python seek_dev_nighters.py
+User id181454262 post your code at 03:38:47
+User RomanGagarin post your code at 02:19:29
+User anatolijplotnikov1 post your code at 02:13:40
+User efirof post your code at 01:45:24
+User potehter post your code at 01:06:43
+User PavelShturm post your code at 00:06:54
+```
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,37 @@
+import requests
+import pytz
+import datetime
+
+
 def load_attempts():
     pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    for page in range(1, pages+1):
+        response = requests.get('http://devman.org/api/challenges/'
+                                'solution_attempts', params={'page': page})
+        json_data = response.json()
+        number_of_pages = json_data['number_of_pages']
+        for record in json_data['records']:
+            yield {
+                   'username': record['username'],
+                   'timestamp': record['timestamp'],
+                   'timezone': record['timezone'],
+                  }
+        if page == number_of_pages:
+            print('This URL has only {} pages and '
+                  'all of them are received'.format(page))
+            break
 
-def get_midnighters():
-    pass
+
+def get_midnighters(users):
+    user_time_zone = pytz.timezone(users['timezone'])
+    timestamp = datetime.datetime.fromtimestamp(int(users['timestamp']))
+    user_time = user_time_zone.localize(timestamp)
+    if 0 <= int(user_time.strftime('%H')) < 6:
+        print('User {} post your code at {}'.format(users['username'],
+                                                    user_time.strftime
+                                                    ('%H:%M:%S')))
+
 
 if __name__ == '__main__':
-  pass
+    for user in load_attempts():
+        get_midnighters(user)
